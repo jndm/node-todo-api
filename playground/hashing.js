9f37421fe64d9936bb1b59761e507f69f238820c
Joonas Meriläinen <joonas.merilainen@gofore.com>
2017-11-13 14:34:28 +0200

    POST /users route: Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,4 +1,6 @@
-// const {SHA256} = require('crypto-js');
+//const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
@@ -26,14 +28,27 @@
 //     console.log('Data was changed. Don\'t trust!')
 // }
 
-const jwt = require('jsonwebtoken');
 
-var data = {
-    id: 10
-};
+// var data = {
+//     id: 10
+// };
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
+
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$wgIQvcZI/ID2Ae3TAESGDejnR4QsZv8ChCW7Ixe5dtg8uU/lxqayK';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
