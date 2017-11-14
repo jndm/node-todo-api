4bc5a5565187ea5fffab8cb68123d9824a9669ec
Joonas Meriläinen <joonas.merilainen@gofore.com>
2017-11-14 09:35:30 +0200

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -175,6 +175,14 @@ app.post('/users/login', (req, res) => {
         });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
